002d40dfebacece765e94db114e63cb55e6e6d0a
Bronson Holland <baholland@student.fullsail.edu>
2020-05-31 08:03:23 -0700

    initial commit

---
+++ BronsonHolland_CE07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BronsonHolland_CE07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
