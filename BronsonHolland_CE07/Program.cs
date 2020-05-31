fe67187fc58f51608b55ebee83cc638bcebc9873
Bronson Holland <baholland@student.fullsail.edu>
2020-05-31 08:58:29 -0700

    data files added

--- BronsonHolland_CE07/Program.cs
+++ BronsonHolland_CE07/Program.cs
@@ -6,7 +6,39 @@ namespace BronsonHolland_CE07
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("1. Datafile1\n"
+                + "2. Datafile2\n"
+                + "3. Datafile3\n"
+                + "4. Exit\n"
+                + "Please choose a file: ");
+            string input = Console.ReadLine().ToLower();
+
+            switch (input)
+            {
+                case "1":
+                case "datafile1":
+                    {
+
+                    }
+                    break;
+                case "2":
+                case "datafile2":
+                    {
+
+                    }
+                    break;
+                case "3":
+                case "datafile3":
+                    {
+
+                    }
+                    break;
+                case "4":
+                case "exit":
+                    {
+                        return;
+                    }
+            }
         }
     }
 }
